5dacf8c2023f083353d97625c28ff6b4eeb2a45f
Stefan Prasse <prasse.stefan@gmx.de>
2017-04-30 14:31:44 +0200

    fix upload in sync service

--- NextcloudAppLibrary/Services/SyncService.cs
+++ NextcloudAppLibrary/Services/SyncService.cs
@@ -572,7 +572,7 @@ namespace NextcloudApp.Services
                 var targetStream = stream.AsStreamForRead();
 
                 IProgress<WebDavProgress> progress = new Progress<WebDavProgress>(ProgressHandler);
-                await client.Upload(path, targetStream, localFile.ContentType, progress, _cts.Token);
+                result = await client.Upload(path, targetStream, localFile.ContentType, progress, _cts.Token);
             }
 
             // Let Windows know that we're finished changing the file so
